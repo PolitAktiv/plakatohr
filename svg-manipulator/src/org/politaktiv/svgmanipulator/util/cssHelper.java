558aeeab18182aa77f40fa43d3f12d133d4914dc
Niels Ott <niels.ott@integrata-stiftung.de>
2017-08-07 16:25:49 +0200

    cssHelper dokumentiert

--- svg-manipulator/src/org/politaktiv/svgmanipulator/util/cssHelper.java
+++ svg-manipulator/src/org/politaktiv/svgmanipulator/util/cssHelper.java
@@ -7,11 +7,16 @@ import java.util.List;
 import java.util.Set;
 
 /**
- *  Tools for handling CSS in SVG files
+ *  Tools for handling CSS as found in in SVG files (not as found as in HTML)
  *
  */
 public class cssHelper {
 	
+	/**
+	 * Splits CSS data from a style attribute into a map of key-value pairs
+	 * @param css the CSS data
+	 * @return a map of key value pairs.
+	 */
 	public static HashMap<String,String> splitCss(String css) {
 		
 		HashMap<String, String> result = new HashMap<String, String>();
@@ -27,6 +32,11 @@ public class cssHelper {
 		
 	}
 	
+	/**
+	 * Takes a map of key-value pairs and re-combines them to a string usable for a style attribute
+	 * @param map map to use
+	 * @return the single string style attribute.
+	 */
 	public static String CssToString(HashMap<String,String> map) {
 		
 		Set<String> elements = new HashSet<String>();
@@ -43,24 +53,31 @@ public class cssHelper {
  		}
 		result = result.replaceFirst(";", "");
 		
-		//System.err.println(result);
-		
-		
 		return (result);
 		
 	}
 	
 	/**
-	 * Takes the value from b and overrides or inserts those in a, returning the result 
-	 * @param a
-	 * @param b
-	 * @return
+	 * Takes values from key-value map <strong>a</strong> and overrides those in key-value map <strong>a</strong>.
+	 * Values not present in <strong>a</strong> will be inserted. 
+	 * @param a the original key-value-map
+	 * @param b the map from which stuff is overwritten in a, or added to a.
+	 * @return the result of all of this magic.
 	 */
 	public static HashMap<String,String> addAndOverride(HashMap<String,String> a, HashMap<String,String> b) {
 		return( addAndOverride(a, b, null));
 		
 	}
 	
+	/**
+	 * Takes values from key-value map <strong>a</strong> and overrides those in key-value map <strong>a</strong>.
+	 * Values not present in <strong>a</strong> will be inserted. <strong>prefix</strong> specifies a prefix for key names
+	 * in <strong>a</strong> that is used as a selector.
+	 * @param a the original key-value-map
+	 * @param b the map from which stuff is overwritten in a, or added to a.
+	 * @param prefix only keys with this prefix will be used from b.
+	 * @return the result of all of this magic.
+	 */
 	public static HashMap<String,String> addAndOverride(HashMap<String,String> a, HashMap<String,String> b, String prefix) {
 		HashMap<String, String> result = new HashMap<String,String>();
 		result.putAll(a);
